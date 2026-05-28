7a36d42fc2acd422faabc5cbf23e1d62e841613a
ReiOle <oserraprof@gmail.com>
2026-05-28 23:41:05 +0200

    main cors

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 # from typing import Union
 from fastapi import FastAPI
 from src.infrastructure.adapters.routes import item_routes, user_routes, auction_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://crownandtrade.com",
+        "https://www.crownandtrade.com",
+        "http://localhost:8100",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_routes.router)
 app.include_router(item_routes.router)
